6c39703dbcd620cdd84e7a8fb836bc31fdce0913
scott.waring@gmail.com <scott.waring@gmail.com>
2018-08-11 20:05:53 +0000

    Done.

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -1,12 +1,35 @@
-arr = []
-arr1 = [info]
-
-arr << number
-
-arr.each {|ele| puts ele}
-
-arr.each_with_index do |ele2, idx|
-    if idx == 2
-      puts ele2
-    end
+def instantiate_new_array
+  arr =[]
+  return arr
 end
+
+def array_with_two_elements
+  arr = []
+  while arr.length <= 2
+    arr << [true, false]*2
+  end
+  return arr
+end
+
+def first_element(arr)
+  return arr[0]
+end
+
+def third_element(arr)
+  return arr[2]
+end
+
+def last_element(arr)
+ return arr[-1]
+end
+
+def first_element_with_array_methods(arr)
+  return arr.shift
+end
+def last_element_with_array_methods(arr)
+  return arr.pop
+end
+def length_of_array(arr)
+  return arr.length
+end
+  
