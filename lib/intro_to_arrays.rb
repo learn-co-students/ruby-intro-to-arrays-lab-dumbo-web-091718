3f5849d941108571eb5640728026867f5ed918ae
soohoo05 <tyler.soohoo@gmail.com>
2018-09-05 09:56:12 -0400

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,39 @@
+def instantiate_new_array
+array=[]
+return array
+end
+
+def array_with_two_elements
+  array=[1,2]
+  return array
+end
+
+def first_element(array)
+first=array.first
+return first
+end
+
+def third_element(array)
+  third=array[2]
+  return third
+end
+
+def last_element(array)
+  last=array[-1]
+  return last
+end
+
+def first_element_with_array_methods(array)
+  first=array.first
+  return first
+  end
+
+  def last_element_with_array_methods(array)
+last=array.last
+return last
+end
+
+def length_of_array(array)
+length=array.count
+return length
+end
